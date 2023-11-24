111ac304ebdb26cba7b713d8c586b2a1356cfca5
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-11-24 12:20:30 +0100

    Stepik - lab06zad2

---
+++ lab06/zad2/README.md
@@ -0,0 +1,9 @@
+### _Zadanie 2_
+
+Dodaj do widoku nowy paragraf, który będzie zawierał poniższą wiadomość:
+
+```
+Witaj [name]!
+
+od Twojej rejestracji minęło [...] dni.
+```
---
+++ lab06/zad2/src/main/java/com/stepik/zad2/Zad2Application.java
@@ -0,0 +1,13 @@
+package com.stepik.zad2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zad2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zad2Application.class, args);
+	}
+
+}
